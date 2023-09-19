c7d93969a18322d5bdf4e8c5e10586e4966560fb
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2023-09-19 13:33:29 +0800

    update lib

--- main.dart
+++ main.dart
@@ -1,1 +1,3 @@
 //nothing here
+//code can't be shared as there are some security regarding Firebase API key, openAI api key and several other reasons
+//the APK can be downloaded here, and can view the website version at terapi.vercel.app
